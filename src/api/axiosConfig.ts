0bb2a6c51c05e77dceb5dd09f97427c2a7e0c59a
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-08-22 20:01:23 +0200

    added axiosConfig for axios instances for HTTP requests

---
+++ src/api/axiosConfig.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
+const axiosInstance = axios.create ({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+
+    },
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+    (Response) => Response,
+    (error) => {
+        if (error.response) {
+            return Promise.reject(new Error(error.response.data?.message || error.message));
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance
