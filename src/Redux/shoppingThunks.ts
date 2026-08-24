7f4bbfd37d92090bca32a11715f7343792a346ea
NeeloByron <byronnkhuna@gmail.com>
2026-08-24 10:21:15 +0200

    complete shoppingthunks file for fetchlist, addlist, updatelist & deletelist

--- src/Redux/shoppingThunks.ts
+++ src/Redux/shoppingThunks.ts
@@ -2,18 +2,53 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { shoppingService } from '@/Service/ShoppingService';
 import type { ShoppingListInput } from '@/Redux/shoppingTypes'
 import type { RootState } from '@/Redux/store'
-import { use } from 'react';
 
 export const fetchLists = createAsyncThunk (
     'shopping/fetchLists',
-    async (_, { getState, rejectWithvalue }) => {
+    async (_, { getState, rejectWithValue }) => {
         try {
             const state = getState() as RootState;
             const userId = state.auth.user?.id;
             if (!userId) throw new Error('Not authenticated');
             return await shoppingService.fetchLists(userId);
         } catch (error: any) {
-           return rejectWithvalue(error.message || 'Failed to fetch list')
+           return rejectWithValue(error.message || 'Failed to fetch list')
         }
+    }
+);
+
+export const addList = createAsyncThunk(
+    'shopping/addList',
+    async (data: ShoppingListInput, { getState, rejectWithValue }) => {
+        try {
+            const state = getState() as RootState;
+            const userId = state.auth.user?.id;
+            if (!userId) throw new Error('Not authenticated');
+            return await shoppingService.addList(userId, data);
+        } catch (error: any) {
+            return rejectWithValue(error.message || 'Failed to add list');
+        }
+    }
+);
+
+export const updatelist = createAsyncThunk(
+    'shopping/updateList',
+    async ({ id, data }: { id: String; data: ShoppingListInput}, { rejectWithValue }) => {
+        try {
+            return await shoppingService.updateList(id, data);
+        } catch (error: any) {
+            return rejectWithValue(error.message || 'Failed to update list');
+        }
+    }
+);
+
+export const deleteList = createAsyncThunk(
+    'shopping/deleteList',
+    async (id: string, { rejectWithValue }) => {
+       try {
+          return await shoppingService.deleteList(id);
+       } catch (error: any) {
+        return rejectWithValue(error.message || 'Failed to delete list');
+       }
     }
 );
